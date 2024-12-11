c5742a62dad4fb6fad6d8a5488eac1c20c74d4e9
VarunRele <rele.varun1@gmail.com>
2024-12-11 18:29:12 +0530

    Static files

--- fuel_log/settings.py
+++ fuel_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -132,3 +133,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'fuel:index'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
